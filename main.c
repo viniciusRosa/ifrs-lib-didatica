b5dd3c71e77b1e0a42d27b2f92698e0f0cdfab3b
johnreis <lenooreis@gmail.com>
2015-07-13 16:28:41 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ifrslib();
+
+void main(){
+    int var_int = leInt("Digite o númerozin: \n");
+    printf("Int : %d\n",var_int);
+    float var_float = leFloat("Digite um float:\n");
+    printf("Float : %f\n",var_float);
+    char var_char = leChar("Digite um char: \n");
+    printf("Char : %c\n",var_char);
+    char var_string = leString("Digite uma String: \n");
+    printf("Int : %s",var_string);
+}
