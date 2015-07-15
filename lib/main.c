384fe1314e754046a0353605ff790e056113808f
marcelogm <marcelogm94@hotmail.com>
2015-07-15 11:50:37 -0300

    Updates

--- lib/main.c
+++ lib/main.c
@@ -2,12 +2,16 @@
 #include "ifrslib.h"
 
 void main(){
+    // RECEBE VALOR INTEIRO
     int var_int = leInt("Digite o númerozin: \n");
     printf("Int : %d\n",var_int);
+    // RECEBE VALOR COM PONTO FLUTUANTE
     float var_float = leFloat("Digite um float:\n");
     printf("Float : %f\n",var_float);
+    // RECEBE CHAR
     char var_char = leChar("Digite um char: \n");
     printf("Char : %c\n",var_char);
+    // RECEBE ARRAY DE CHAR
     char *var_string = leString("Digite uma String: \n");
     printf("String : %s\n",var_string);
 }
