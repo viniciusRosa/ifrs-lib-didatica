cfaf3e1b4a26609398fae9285ea9cf844dc38305
marcelogm <marcelogm94@hotmail.com>
2015-09-29 11:15:48 -0300

    29_09_2015
    Funções adicionadas, modificação nas existentes.

--- lib/ifrslib.c
+++ lib/ifrslib.c
@@ -10,52 +10,158 @@
  * de valores primitivos em C
  */
 
-long leInt(char msg[]) {
+
+/*
+ *  FUNÇÕES DE ENTRADA
+ */
+
+int lerInt(char mensagemCabecalho[],
+           char mensagemErro[]){
     int value, ret;
-    printf("%s",msg);
-    ret = scanf("\n%ld",&value);
-    fflush(stdin);                      //SIMILAR AO "getchar();" - LIMPAR BUFFER DE ENTRADA
-    if(ret == 1){                       //AO UTILIZAR "getchar();", A FUNÇÃO REPETIRA A CADA CARACTER
+    printf("%s", mensagemCabecalho);
+    ret = scanf("\n%d",&value);
+    //SIMILAR AO "getchar();" - LIMPAR BUFFER DE ENTRADA
+    //AO UTILIZAR "getchar();", A FUNÇÃO REPETIRA A CADA CARACTER
+    fflush(stdin);
+
+    if(ret == TRUE){
         return value;
     }else{
-        printf("Erro! Somente numeros.\n");
-        leInt(msg);
+        printf("%s", mensagemErro);
+        lerInt(mensagemCabecalho, mensagemErro);
     }
 }
 
-float leFloat(char msg[]){
+long lerLong(char mensagemCabecalho[],
+             char mensagemErro[]){
+    long value;
+    int ret;
+    printf("%s", mensagemCabecalho);
+    ret = scanf("\n%ld",&value);
+    //SIMILAR AO "getchar();" - LIMPAR BUFFER DE ENTRADA
+    //AO UTILIZAR "getchar();", A FUNÇÃO REPETIRA A CADA CARACTER
+    fflush(stdin);
+
+    if(ret == TRUE){
+        return value;
+    }else{
+        printf("%s", mensagemErro);
+        lerLong(mensagemCabecalho, mensagemErro);
+    }
+}
+
+float lerFloat(char mensagemCabecalho[],
+               char mensagemErro[]){
     float value;
-    printf("%s",msg);
-    scanf("%f",&value);
-    return value;
+    int ret;
+    printf("%s", mensagemCabecalho);
+    ret = scanf("%f",&value);
+    fflush(stdin);
+
+    if(ret == TRUE){
+        return value;
+    }else{
+        printf("%s", mensagemErro);
+        lerLong(mensagemCabecalho, mensagemErro);
+    }
 }
 
-char leChar(char msg[]){
+char lerChar(char mensagemCabecalho[]){
     char value;
-    printf("%s",msg);
-    scanf("\n%c",&value);
+    printf("%s", mensagemCabecalho);
+    value = fgetc(stdin);
+    fflush(stdin);
     return value;
 }
 
-char *leString(char msg[]){
-    char *value[255];
-    printf("%s",msg);
-    getchar();
-    scanf("%254[^\n]s", value);         //PARAMETRO DO SCANF QUE IMPEDE O OVERFLOW
-    return value;                       //E TERMINA A EXECUÇÃO COM O NEWLINE
+char *lerString(char mensagemCabecalho[],
+                int tamanhoDaString){
+    char *value = (char *) malloc(tamanhoDaString * sizeof(char));
+    printf("%s", mensagemCabecalho);
+    fgets(value, tamanhoDaString, stdin);
+    fflush(stdin);
+    return value;
 }
 
-int imprimeMatrizIntB(){
-
-}
-
-char *trocaChar(char text[], char changeit, char changeto){
+char **lerArrayDeString(char mensagemCabecalho[],
+                        char mensagemElemento[],
+                        int quantidadeDeElemento,
+                        int tamanhoDaString){
+    printf("%s",mensagemCabecalho);
+    char ** strings = (char **) malloc(quantidadeDeElemento *
+                                       tamanhoDaString *
+                                       sizeof(char));
     int i;
-    for(i=0;i<=strlen(text);i++){
-        if(text[i]==changeit){
-            text[i]=changeto;
+    for (i=0; i < quantidadeDeElemento; i++){
+        *(strings+i) = lerString(mensagemElemento,
+                                 tamanhoDaString);
+    }
+    return strings;
+}
+
+/*
+ *  FUNÇÕES DE SAÍDA
+ */
+
+void imprimirInt(char mensagemCabecalho[],
+                 int valor){
+    printf("%s", mensagemCabecalho);
+    printf("%d\n", valor);
+}
+
+void imprimirLong(char mensagemCabecalho[],
+                  long valor){
+    printf("%s", mensagemCabecalho);
+    printf("%ld\n", valor);
+}
+
+void imprimirFloat(char mensagemCabecalho[],
+                   float valor){
+    printf("%s", mensagemCabecalho);
+    printf("%f\n", valor);
+}
+
+void imprimirChar(char mensagemCabecalho[],
+                  char valor){
+    printf("%s", mensagemCabecalho);
+    printf("%c\n", valor);
+}
+
+void imprimirString(char mensagemCabecalho[],
+                    char valor[]){
+    printf("%s", mensagemCabecalho);
+    printf("%s\n", valor);
+}
+
+void imprimirArrayDeStrings(char mensagemCabecalho[],
+                            char mensagemElemento[],
+                            char **arrayDeStrings){
+    printf("%s", mensagemCabecalho);
+    int i = 0;
+    while(*(arrayDeStrings+i)){
+        printf("%s", mensagemElemento);
+        int j = 0;
+        while (*(*(arrayDeStrings+i)+j)){
+            printf("%c", *(*(arrayDeStrings+i)+j));
+            j++;
+        }
+        i++;
+    }
+    puts("\n");
+}
+
+/*
+ * UTILITARIOS E
+ * ALGORITMOS IMPORTANTES
+ */
+
+char *trocaChar(char texto[], char mudeIsso, char paraIsso){
+    int i;
+    for(i=0;i<=strlen(texto);i++){
+        if(texto[i]==mudeIsso){
+            texto[i]=paraIsso;
         }
     }
-    return text;
+    return texto;
 }
 
--- lib/main.c
+++ lib/main.c
@@ -3,27 +3,46 @@
 
 void main(){
 
-    // RECEBE VALOR INTEIRO
-    int var_int = leInt("Digite o númerozin: \n");
-    printf("Int : %d\n",var_int);
+    // RECEBE E IMPRIME UM INTEIRO
+    int var_int = lerInt("Digite um Inteiro: \n",
+                         "Erro! Digite somente Inteiros:\n");
+    imprimirInt("Inteiro:\n",var_int);
 
-    // RECEBE VALOR COM PONTO FLUTUANTE
-    float var_float = leFloat("Digite um float:\n");
-    printf("Float : %f\n",var_float);
+    // RECEBE E IMPRIME UM LONG
+    long var_long = lerLong("Digite um Long: \n",
+                            "Erro! Digite somente Long:\n");
+    imprimirLong("Long:\n",var_long);
 
-    // RECEBE CHAR
-    char var_char = leChar("Digite um char: \n");
-    printf("Char : %c\n",var_char);
+    // RECEBE E IMPRIME UM PONTO FLUTUANTE
+    // APRESENTA PROBLEMAS DE PRECISÃO
+    float var_float = lerFloat("Digite um Float: \n",
+                                "Erro! Digite somente Float:\n");
+    imprimirFloat("Float:\n", var_float);
 
-    // RECEBE ARRAY DE CHAR
-    char *var_string = leString("Digite uma String: \n");
-    printf("String : %s\n",var_string);
+    // RECEBE E IMPRIME UM CHAR
+    char var_char = lerChar("Digite um Char: \n");
+    imprimirChar("Char:\n",var_char);
+
+    // RECEBE E IMPRIME UMA STRING
+    char *var_string = lerString("Digite uma String: \n",
+                                 255);
+    imprimirString("String:\n",var_string);
+
+    // RECEBE E IMPRIME ARRAY DE STRINGS
+
+    char **var_arrayString = lerArrayDeString("Lendo array de strings!\n",
+                                              "Digite o conteudo da string:\n",
+                                              3,
+                                              255);
+    imprimirArrayDeStrings("Array de Strings:\n",
+                           "",
+                           var_arrayString);
 
     // FUNÇÃO DE TROCA DE LETRAS
+
     char textToChange[] = "THE QUICK BROWN FOX JIUMPS OVER THE LAZY DOG";
     printf("\nTexto antes: \n%s", textToChange);
     *trocaChar(textToChange, 'O', 'A');
     printf("\nTexto depois: \n%s\n",textToChange);
 
-    //
 }
